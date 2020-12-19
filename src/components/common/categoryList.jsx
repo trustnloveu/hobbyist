1b1a3a43da6139c25aa90b09548f707cbfb8c0af
Euijin Yang <trustnloveu@gmail.com>
2020-12-19 12:16:38 +0900

    Add style factors which is relating to hovering when cursor point category list

--- src/components/common/categoryList.jsx
+++ src/components/common/categoryList.jsx
@@ -1,29 +1,40 @@
 import React, { useState } from "react";
 
 const Li = ({ context }) => {
-  const [hover, setHover] = useState(false);
-  // const [style, setStyle] = useState();
-  const style = {
-    width: "150px",
-    display: "inline-block",
-    textAlign: "center",
-    height: "50px",
-    border: "1px solid #eee",
-    borderRadius: "5px",
-    margin: "10px",
-    lineHeight: "50px",
-    backgroundColor: "#eee",
+  // const [hover, setHover] = useState(false);
+
+  const styleSheet = {
+    normal: {
+      width: "150px",
+      display: "inline-block",
+      textAlign: "center",
+      height: "50px",
+      border: "1px solid #eee",
+      borderRadius: "5px",
+      margin: "10px",
+      lineHeight: "50px",
+      backgroundColor: "#eee",
+    },
+    hover: {
+      transition: "1.0s",
+      backgroundColor: "#ccc",
+      cursor: "pointer",
+    },
   };
+  const [style, setStyle] = useState({ ...styleSheet.normal });
 
   return (
     <li
       style={style}
-      // onMouseOver={() => {
-      //   setHover(true);
-      // }}
-      // onMouseLeave={() => {
-      //   setHover(false);
-      // }}
+      onMouseOver={() => {
+        setStyle({ ...styleSheet.normal, ...styleSheet.hover });
+        // setHover(true);
+      }}
+      onMouseLeave={() => {
+        setStyle({ ...styleSheet.normal });
+        // setHover(false);
+      }}
+      onClick={() => console.log(`clicked`)}
     >
       {context}
     </li>
