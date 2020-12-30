3686c77ec36017ae75b91e023533279d9490648d
Euijin Yang <trustnloveu@gmail.com>
2020-12-30 20:19:30 +0900

    Get group data from the server. Pass it to Group comonent and Align it through id parameter

--- src/components/group.jsx
+++ src/components/group.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getGroup } from "../services/groupService";
 
-const Group = () => {
-  return <div>group</div>;
+const Group = (props) => {
+  // state
+  const [group, setGroup] = useState({});
+
+  // useEffect
+  useEffect(() => {
+    // getSingleGroup
+    async function getSingleGroup() {
+      const { data } = await getGroup(props.match.params.id);
+      setGroup(data);
+    }
+
+    getSingleGroup();
+  }, [props.match.params.id]);
+
+  // return
+  return { group } && <div>{group.title}</div>;
 };
 
 export default Group;
